ef4bb86c1ce13ac6ca44757e3872964c1ee209f6
javier-altmann <javier.altmann@hotmail.com>
2017-08-22 17:38:50 -0300

    Inscripción de alumnos
    Finalizo inscripcion de alumnos

--- Views/inscribir_alumno.php
+++ Views/inscribir_alumno.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Inscribir Alumno</title>
+    <title>Inscribir Alumnos</title>
 
     <link rel="stylesheet" href="../css/grid.css">
    
@@ -13,15 +13,58 @@
      <? include 'menu_secreteria.php'; ?>
 </header>
 <body>
-  
+   <form action="inscribir_alumno.php" method="POST">
    <section style="text-align: center; margin-top:50px;">
   
+    <div class="col-8">
+        <label>Materia</label>
+         <select name="materia">
+          <?
+           require_once 'conexion.php';
+             $sql = "SELECT * from asignatura";
+            $resultado = $con->query($sql);
+             ?>
+         <? foreach($resultado as $row){ ?>
+          <option value="<? echo $row['id_asignatura']; ?>"><? echo $row['nombre_asignatura']; ?></option>
+          <?}?>
+        
+        </select>
+ 
+  
+   
+    </div>
 
+    <div class="col-8" style="margin-top:10px;">
+        <label>Alumno</label>
+         <select name="alumno">
+          <?
+           require_once 'conexion.php';
+             $sql = "SELECT * from alumno";
+            $resultado = $con->query($sql);
+             ?>
+         <? foreach($resultado as $row){ ?>
+          <option value="<? echo $row['id_alumno']; ?>"><? echo $row['nombre']. $row['apellido']; ?></option>
+          <?}?>
+        
+        </select>
+ 
+    </div>
+       
 
-      
-    </section>
+    <input type="submit" name="inscribir_alumno" value="Inscribir Alumno" style="margin-top:7px";>
     
+    </section>
+
+     </form>
    
 </body>
 </html>
- 
+<?php
+
+    $id_asignatura = $_POST['materia']; 
+    $id_alumno= $_POST['alumno'];
+    $sql = "INSERT INTO cursada (id_cursada, id_asignatura, id_alumno) VALUES (NULL, '$id_asignatura', '$id_alumno')";
+    $con->exec($sql);
+
+?>
+          

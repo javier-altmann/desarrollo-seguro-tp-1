fbbac59f3328941aec2d78af7cb4b13e1d0c09ca
javier-altmann <javier.altmann@hotmail.com>
2017-08-28 20:45:55 -0300

    Base de datos actualizada
    Subo base de datos actualizada

--- tp01.sql
+++ tp01.sql
@@ -1,232 +1,83 @@
--- phpMyAdmin SQL Dump
--- version 3.5.1
--- http://www.phpmyadmin.net
---
--- Servidor: localhost
--- Tiempo de generación: 24-08-2017 a las 16:11:47
--- Versión del servidor: 5.5.24-log
--- Versión de PHP: 5.4.3
+CREATE TABLE IF NOT EXISTS alumno (
+  id_alumno int(11) NOT NULL AUTO_INCREMENT,
+  nombre varchar(250) DEFAULT NULL,
+  apellido varchar(250) DEFAULT NULL,
+  id_cursada int(11) DEFAULT NULL,
+  PRIMARY KEY (id_alumno),
+  UNIQUE KEY id_alumno (id_alumno),
+  KEY id_cursada (id_cursada)
+);
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Base de datos: `tp01`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `alumno`
---
-
-CREATE TABLE IF NOT EXISTS `alumno` (
-  `id_alumno` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(250) DEFAULT NULL,
-  `apellido` varchar(250) DEFAULT NULL,
-  `id_cursada` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_alumno`),
-  UNIQUE KEY `id_alumno` (`id_alumno`),
-  KEY `id_cursada` (`id_cursada`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
-
---
--- Volcado de datos para la tabla `alumno`
---
-
-INSERT INTO `alumno` (`id_alumno`, `nombre`, `apellido`, `id_cursada`) VALUES
+INSERT INTO alumno (id_alumno, nombre, apellido, id_cursada) VALUES
 (1, 'Javier', 'Altmann', NULL),
 (2, 'Carlos', 'Prueba', NULL);
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `asignatura`
---
+CREATE TABLE IF NOT EXISTS asignatura (
+  id_asignatura int(11) NOT NULL AUTO_INCREMENT,
+  id_usuario int(11) DEFAULT NULL,
+  nombre_asignatura varchar(250) DEFAULT NULL,
+  PRIMARY KEY (id_asignatura),
+  UNIQUE KEY id_asignatura (id_asignatura),
+  KEY id_usuario (id_usuario)
+);
 
-CREATE TABLE IF NOT EXISTS `asignatura` (
-  `id_asignatura` int(11) NOT NULL AUTO_INCREMENT,
-  `id_profesor` int(11) DEFAULT NULL,
-  `nombre_asignatura` varchar(250) DEFAULT NULL,
-  PRIMARY KEY (`id_asignatura`),
-  UNIQUE KEY `id_asignatura` (`id_asignatura`),
-  KEY `id_profesor` (`id_profesor`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=48 ;
 
---
--- Volcado de datos para la tabla `asignatura`
---
 
-INSERT INTO `asignatura` (`id_asignatura`, `id_profesor`, `nombre_asignatura`) VALUES
-(45, 4, 'redes'),
-(46, 4, 'Programacion'),
-(47, 1, 'Administración');
+CREATE TABLE IF NOT EXISTS cursada (
+  id_cursada int(11) NOT NULL AUTO_INCREMENT,
+  id_asignatura int(11) DEFAULT NULL,
+  id_alumno int(11) DEFAULT NULL,
+  PRIMARY KEY (id_cursada),
+  UNIQUE KEY id_cursada (id_cursada),
+  KEY id_asignatura (id_asignatura)
+);
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `cursada`
---
-
-CREATE TABLE IF NOT EXISTS `cursada` (
-  `id_cursada` int(11) NOT NULL AUTO_INCREMENT,
-  `id_asignatura` int(11) DEFAULT NULL,
-  `id_alumno` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_cursada`),
-  UNIQUE KEY `id_cursada` (`id_cursada`),
-  KEY `id_asignatura` (`id_asignatura`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
-
---
--- Volcado de datos para la tabla `cursada`
---
-
-INSERT INTO `cursada` (`id_cursada`, `id_asignatura`, `id_alumno`) VALUES
+INSERT INTO cursada (id_cursada, id_asignatura, id_alumno) VALUES
 (8, 45, 2),
 (9, 45, 1),
 (10, 46, 1),
-(11, 47, 2);
+(11, 47, 2),
+(16, 47, 1);
 
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS notas_alumno (
+  id_nota int(11) NOT NULL AUTO_INCREMENT,
+  id_alumno int(11) DEFAULT NULL,
+  id_asignatura int(11) DEFAULT NULL,
+  nota double DEFAULT NULL,
+  PRIMARY KEY (id_nota),
+  UNIQUE KEY id_nota (id_nota),
+  KEY id_alumno (id_alumno),
+  KEY id_asignatura (id_asignatura)
+);
 
---
--- Estructura de tabla para la tabla `notas_alumno`
---
 
-CREATE TABLE IF NOT EXISTS `notas_alumno` (
-  `id_nota` int(11) NOT NULL AUTO_INCREMENT,
-  `id_alumno` int(11) DEFAULT NULL,
-  `id_asignatura` int(11) DEFAULT NULL,
-  `nota` double DEFAULT NULL,
-  PRIMARY KEY (`id_nota`),
-  UNIQUE KEY `id_nota` (`id_nota`),
-  KEY `id_alumno` (`id_alumno`),
-  KEY `id_asignatura` (`id_asignatura`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+CREATE TABLE usuario (
+	id_usuario int PRIMARY KEY UNIQUE AUTO_INCREMENT, 
+	nombre varchar(250),
+	usuario varchar(250),
+	contraseña varchar (250),
+    rol varchar(255),
+	id_permisos int
+);
 
--- --------------------------------------------------------
+CREATE TABLE permiso(
+	id_permiso int primary key auto_increment unique, 
+	rol varchar(255), 
 
---
--- Estructura de tabla para la tabla `permisos`
---
+ FOREIGN KEY (id_permiso) REFERENCES Usuario(id_permiso)
+);
 
-CREATE TABLE IF NOT EXISTS `permisos` (
-  `id_permisos` int(11) NOT NULL AUTO_INCREMENT,
-  `descripcion` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id_permisos`),
-  UNIQUE KEY `id_permisos` (`id_permisos`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `profesor`
---
-
-CREATE TABLE IF NOT EXISTS `profesor` (
-  `id_profesor` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(250) DEFAULT NULL,
-  `dni` varchar(20) DEFAULT NULL,
-  `id_permisos` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_profesor`),
-  UNIQUE KEY `id_profesor` (`id_profesor`),
-  KEY `id_permisos` (`id_permisos`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
-
---
--- Volcado de datos para la tabla `profesor`
---
-
-INSERT INTO `profesor` (`id_profesor`, `nombre`, `dni`, `id_permisos`) VALUES
-(1, 'Juan', '32712332', NULL),
-(4, 'pedro', '3987102', NULL);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `secretaria`
---
-
-CREATE TABLE IF NOT EXISTS `secretaria` (
-  `id_secretaria` int(11) NOT NULL AUTO_INCREMENT,
-  `id_permisos` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_secretaria`),
-  UNIQUE KEY `id_secretaria` (`id_secretaria`),
-  KEY `id_permisos` (`id_permisos`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios`
---
-
-CREATE TABLE IF NOT EXISTS `usuarios` (
-  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `id_profesor` int(11) DEFAULT NULL,
-  `id_secretaria` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_usuario`),
-  UNIQUE KEY `id_usuario` (`id_usuario`),
-  KEY `id_profesor` (`id_profesor`),
-  KEY `id_secretaria` (`id_secretaria`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `alumno`
---
-ALTER TABLE `alumno`
-  ADD CONSTRAINT `alumno_ibfk_1` FOREIGN KEY (`id_cursada`) REFERENCES `cursada` (`id_cursada`);
-
---
--- Filtros para la tabla `asignatura`
---
 ALTER TABLE `asignatura`
-  ADD CONSTRAINT `asignatura_ibfk_1` FOREIGN KEY (`id_profesor`) REFERENCES `profesor` (`id_profesor`);
+  ADD CONSTRAINT `asignatura_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
 
---
--- Filtros para la tabla `cursada`
---
 ALTER TABLE `cursada`
   ADD CONSTRAINT `cursada_ibfk_1` FOREIGN KEY (`id_asignatura`) REFERENCES `asignatura` (`id_asignatura`);
 
---
--- Filtros para la tabla `notas_alumno`
---
 ALTER TABLE `notas_alumno`
   ADD CONSTRAINT `notas_alumno_ibfk_1` FOREIGN KEY (`id_alumno`) REFERENCES `alumno` (`id_alumno`),
   ADD CONSTRAINT `notas_alumno_ibfk_2` FOREIGN KEY (`id_asignatura`) REFERENCES `asignatura` (`id_asignatura`);
 
---
--- Filtros para la tabla `profesor`
---
-ALTER TABLE `profesor`
-  ADD CONSTRAINT `profesor_ibfk_1` FOREIGN KEY (`id_permisos`) REFERENCES `permisos` (`id_permisos`);
 
---
--- Filtros para la tabla `secretaria`
---
-ALTER TABLE `secretaria`
-  ADD CONSTRAINT `secretaria_ibfk_1` FOREIGN KEY (`id_permisos`) REFERENCES `permisos` (`id_permisos`);
-
---
--- Filtros para la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_profesor`) REFERENCES `profesor` (`id_profesor`),
-  ADD CONSTRAINT `usuarios_ibfk_2` FOREIGN KEY (`id_secretaria`) REFERENCES `secretaria` (`id_secretaria`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

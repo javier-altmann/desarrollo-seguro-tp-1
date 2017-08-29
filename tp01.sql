618e5f6f27775750975a98589df2783c0d734546
javier-altmann <javier.altmann@hotmail.com>
2017-08-29 00:44:43 -0300

    fix bd
    fix bd

--- tp01.sql
+++ tp01.sql
@@ -1,83 +1,230 @@
-CREATE TABLE IF NOT EXISTS alumno (
-  id_alumno int(11) NOT NULL AUTO_INCREMENT,
-  nombre varchar(250) DEFAULT NULL,
-  apellido varchar(250) DEFAULT NULL,
-  id_cursada int(11) DEFAULT NULL,
-  PRIMARY KEY (id_alumno),
-  UNIQUE KEY id_alumno (id_alumno),
-  KEY id_cursada (id_cursada)
-);
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 29-08-2017 a las 03:43:27
+-- Versión del servidor: 5.7.14
+-- Versión de PHP: 5.6.25
 
-INSERT INTO alumno (id_alumno, nombre, apellido, id_cursada) VALUES
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tp01`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumno`
+--
+
+CREATE TABLE `alumno` (
+  `id_alumno` int(11) NOT NULL,
+  `nombre` varchar(250) DEFAULT NULL,
+  `apellido` varchar(250) DEFAULT NULL,
+  `id_cursada` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `alumno`
+--
+
+INSERT INTO `alumno` (`id_alumno`, `nombre`, `apellido`, `id_cursada`) VALUES
 (1, 'Javier', 'Altmann', NULL),
 (2, 'Carlos', 'Prueba', NULL);
 
+-- --------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS asignatura (
-  id_asignatura int(11) NOT NULL AUTO_INCREMENT,
-  id_usuario int(11) DEFAULT NULL,
-  nombre_asignatura varchar(250) DEFAULT NULL,
-  PRIMARY KEY (id_asignatura),
-  UNIQUE KEY id_asignatura (id_asignatura),
-  KEY id_usuario (id_usuario)
-);
+--
+-- Estructura de tabla para la tabla `asignatura`
+--
 
+CREATE TABLE `asignatura` (
+  `id_asignatura` int(11) NOT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `nombre_asignatura` varchar(250) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `asignatura`
+--
 
-CREATE TABLE IF NOT EXISTS cursada (
-  id_cursada int(11) NOT NULL AUTO_INCREMENT,
-  id_asignatura int(11) DEFAULT NULL,
-  id_alumno int(11) DEFAULT NULL,
-  PRIMARY KEY (id_cursada),
-  UNIQUE KEY id_cursada (id_cursada),
-  KEY id_asignatura (id_asignatura)
-);
+INSERT INTO `asignatura` (`id_asignatura`, `id_usuario`, `nombre_asignatura`) VALUES
+(1, 1, 'integracion'),
+(2, 1, 'Redes');
 
-INSERT INTO cursada (id_cursada, id_asignatura, id_alumno) VALUES
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cursada`
+--
+
+CREATE TABLE `cursada` (
+  `id_cursada` int(11) NOT NULL,
+  `id_asignatura` int(11) DEFAULT NULL,
+  `id_alumno` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `cursada`
+--
+
+INSERT INTO `cursada` (`id_cursada`, `id_asignatura`, `id_alumno`) VALUES
 (8, 45, 2),
 (9, 45, 1),
 (10, 46, 1),
 (11, 47, 2),
-(16, 47, 1);
+(16, 47, 1),
+(17, 2, 2);
 
-CREATE TABLE IF NOT EXISTS notas_alumno (
-  id_nota int(11) NOT NULL AUTO_INCREMENT,
-  id_alumno int(11) DEFAULT NULL,
-  id_asignatura int(11) DEFAULT NULL,
-  nota double DEFAULT NULL,
-  PRIMARY KEY (id_nota),
-  UNIQUE KEY id_nota (id_nota),
-  KEY id_alumno (id_alumno),
-  KEY id_asignatura (id_asignatura)
-);
+-- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `notas_alumno`
+--
 
-CREATE TABLE usuario (
-	id_usuario int PRIMARY KEY UNIQUE AUTO_INCREMENT, 
-	nombre varchar(250),
-	usuario varchar(250),
-	contraseña varchar (250),
-    rol varchar(255),
-	id_permisos int
-);
+CREATE TABLE `notas_alumno` (
+  `id_nota` int(11) NOT NULL,
+  `id_alumno` int(11) DEFAULT NULL,
+  `id_asignatura` int(11) DEFAULT NULL,
+  `nota` double DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-CREATE TABLE permiso(
-	id_permiso int primary key auto_increment unique, 
-	rol varchar(255), 
+-- --------------------------------------------------------
 
- FOREIGN KEY (id_permiso) REFERENCES Usuario(id_permiso)
-);
+--
+-- Estructura de tabla para la tabla `permiso`
+--
 
+CREATE TABLE `permiso` (
+  `id_permiso` int(11) NOT NULL,
+  `rol` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `permiso`
+--
 
+INSERT INTO `permiso` (`id_permiso`, `rol`) VALUES
+(1, 'profesor'),
+(2, 'secretaria');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(250) DEFAULT NULL,
+  `usuario` varchar(250) DEFAULT NULL,
+  `contrasenia` varchar(250) DEFAULT NULL,
+  `rol` varchar(255) DEFAULT NULL,
+  `id_permisos` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id_usuario`, `nombre`, `usuario`, `contrasenia`, `rol`, `id_permisos`) VALUES
+(1, 'Javier', 'javier.altmann', 'javier', 'profesor', 1),
+(2, 'Emanuel', 'Emanuel.paz', 'emanuel', 'profesor', 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  ADD PRIMARY KEY (`id_alumno`),
+  ADD UNIQUE KEY `id_alumno` (`id_alumno`),
+  ADD KEY `id_cursada` (`id_cursada`);
+
+--
+-- Indices de la tabla `asignatura`
+--
 ALTER TABLE `asignatura`
-  ADD CONSTRAINT `asignatura_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
+  ADD PRIMARY KEY (`id_asignatura`),
+  ADD UNIQUE KEY `id_asignatura` (`id_asignatura`),
+  ADD KEY `id_usuario` (`id_usuario`);
 
+--
+-- Indices de la tabla `cursada`
+--
 ALTER TABLE `cursada`
-  ADD CONSTRAINT `cursada_ibfk_1` FOREIGN KEY (`id_asignatura`) REFERENCES `asignatura` (`id_asignatura`);
+  ADD PRIMARY KEY (`id_cursada`),
+  ADD UNIQUE KEY `id_cursada` (`id_cursada`),
+  ADD KEY `id_asignatura` (`id_asignatura`);
 
+--
+-- Indices de la tabla `notas_alumno`
+--
 ALTER TABLE `notas_alumno`
-  ADD CONSTRAINT `notas_alumno_ibfk_1` FOREIGN KEY (`id_alumno`) REFERENCES `alumno` (`id_alumno`),
-  ADD CONSTRAINT `notas_alumno_ibfk_2` FOREIGN KEY (`id_asignatura`) REFERENCES `asignatura` (`id_asignatura`);
+  ADD PRIMARY KEY (`id_nota`),
+  ADD UNIQUE KEY `id_nota` (`id_nota`),
+  ADD KEY `id_alumno` (`id_alumno`),
+  ADD KEY `id_asignatura` (`id_asignatura`);
 
+--
+-- Indices de la tabla `permiso`
+--
+ALTER TABLE `permiso`
+  ADD PRIMARY KEY (`id_permiso`),
+  ADD UNIQUE KEY `id_permiso` (`id_permiso`);
 
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `id_usuario` (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  MODIFY `id_alumno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `asignatura`
+--
+ALTER TABLE `asignatura`
+  MODIFY `id_asignatura` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `cursada`
+--
+ALTER TABLE `cursada`
+  MODIFY `id_cursada` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+--
+-- AUTO_INCREMENT de la tabla `notas_alumno`
+--
+ALTER TABLE `notas_alumno`
+  MODIFY `id_nota` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `permiso`
+--
+ALTER TABLE `permiso`
+  MODIFY `id_permiso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
